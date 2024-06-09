33a541ba8f93f3fc4113f0dd2a4f2ed0a9dfefa2
bbvenega <bbvenega@asu.edu>
2024-06-08 17:40:59 -0700

    It's Alive!
    *Initialized Project w/ Spring Boot and React

---
+++ fantasy-sports-app/src/main/java/com/brianvenegas/fantasysports/fantasy_sports_app/FantasySportsAppApplication.java
@@ -0,0 +1,14 @@
+package com.brianvenegas.fantasysports.fantasy_sports_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasySportsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasySportsAppApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ fantasy-sports-app/src/test/java/com/brianvenegas/fantasysports/fantasy_sports_app/FantasySportsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brianvenegas.fantasysports.fantasy_sports_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasySportsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
